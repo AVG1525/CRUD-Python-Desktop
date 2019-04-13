a3a3f9e83245601a639b6ab9df333a2a189e5057
Aron <aronvinicius@live.com.pt>
2019-04-13 03:10:53 -0300

    Biblioteca do Projeto

---
+++ Bibliotecas.py
@@ -0,0 +1,8 @@
+#Todoas as bibliotecas do código ficarão aqui:
+
+# Biblioteca para execução no terminal, utilizando comandos do terminal
+import os
+
+
+# Biblioteca para execuçãod de Criptografia, para TABLE restricao, MD5()
+import hashlib
